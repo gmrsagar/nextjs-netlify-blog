7aaceae6733e2e7b9085fd7433d44b323a624b63
Sagar Ghimire <gmrsagar@gmail.com>
2020-09-22 23:49:28 +1000

    Update author and basics

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,10 @@ export default function Index() {
       <div className="container">
         <div>
           <h1>
-            Hi, We're Next.js & Netlify<span className="fancy">.</span>
+            Hi, I'm Sagar<span className="fancy">.</span>
           </h1>
-          <span className="handle">@nextjs-netlify-blog</span>
-          <h2>A blog template with Next.js and Netlify.</h2>
+          <span className="handle">@gmrsagar</span>
+          <h2>Blogging at it's best</h2>
           <SocialList />
         </div>
       </div>
